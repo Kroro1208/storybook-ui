f64861ac554c14bd84941543da682c93d2092ae4
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-18 20:19:24 +0900

    taileindの設定

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+
+const Button = () => {
+  return (
+    <button type="button" className="px-4 py-3 rounded-lg bg-blue-500 text-white">
+      ボタンコンポーネント
+    </button>
+  )
+}
+
+export default Button
