6ff61ace021bb6db9a6bb752ffe3799c88002331
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-18 21:51:49 +0900

    ライブラ入り使用して各種ボタン表示

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,64 @@
-import type { ComponentProps } from "react"
+import { cn } from "@/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+import { forwardRef, type ComponentProps } from "react"
 
-type ButtonProps = ComponentProps<"button">;
+const buttonStyle = cva([
+    "w-full",
+    "rounded-md",
+    "font-semibold",
+    "focus:outline-none",
+    "disable:cursor-not-allowed"
+    ],
+    {
+        variants: {
+            variant: {
+                solid: "",
+                outline: "border-2",
+                ghost: "transition-colors duration-300"
+            },
+            size: {
+                sm: "px-4 py-2 text-sm",
+                md: "px-4 py-2 text-base",
+                lg: "px-4 py-2 text-lg",
+            },
+            colorscheme: {
+                primary: "text-white"
+            }
+        },
+        compoundVariants: [
+            {
+              variant: "solid",
+              colorscheme: "primary",
+              className: "bg-primary-500 hover:bg-primary-600",
+            },
+            {
+              variant: "outline",
+              colorscheme: "primary",
+              className:
+                "text-primary-600 border-primary-500 bg-transparent hover:bg-primary-100",
+            },
+            {
+              variant: "ghost",
+              colorscheme: "primary",
+              className: "text-primary-600 bg-transparent hover:bg-primary-100",
+            },
+          ],
+          defaultVariants: {
+            variant: "solid",
+            size: "md",
+            colorscheme: "primary",
+          },
+    }
+);
 
-const Button = ({...props }: ButtonProps) => {
-  return (
-    <button className="px-4 py-2 rounded-full bg-green-500 text-white" {...props} />
-  )
-}
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyle>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    variant, size, colorscheme, className, ...props }, ref) => {
+    return (
+      <button ref={ref} className={cn(buttonStyle({variant, size, colorscheme}))} {...props} />
+    )
+
+})
 
 export default Button
