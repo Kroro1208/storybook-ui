2b03f472d55170e38f1ce991228e4340156efb49
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-18 21:24:28 +0900

    tailwindのライブラリインストール

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,9 +4,7 @@ type ButtonProps = ComponentProps<"button">;
 
 const Button = ({...props }: ButtonProps) => {
   return (
-    <button className="px-4 py-2 rounded-full bg-green-500 text-white" {...props}>
-        Button
-    </button>
+    <button className="px-4 py-2 rounded-full bg-green-500 text-white" {...props} />
   )
 }
 
